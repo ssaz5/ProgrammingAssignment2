0901a6799e13045119b699a0132ab84e5e9817c9
ssaz5 <ssaz_5@live.com>
2016-06-13 14:34:34 +0200

    Submission Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+## makeCacheMatix 
+## creates a matrix which can save its inverse in memory
 
+makeCacheMatrix <- function(M = matrix()){
+    # Give a warning in case the dimensions are wrong
+    if(dim(M)[1] != dim(M)[2]){
+        print("Warning: Matrix is not a Square Matrix")
+    }
+    # initialize INV as NULL
+    INV <-NULL
+    
+    
+    set <- function(x){ # set value of M
+        if(dim(x)[1] != dim(x)[2]){
+            print("Warning: Matrix is not a Square Matrix")
+        }
+        M <<-x
+        INV <<- NULL
+        
+    }
+    get <- function() M # read value of M
+    setINV <- function(inverse) INV <<- inverse # set the cached INV variable
+    getINV <- function() INV # read the cached inverse variable
+    
+    list(set = set, get = get, setINV = setINV, getINV = getINV)
+    
+    
+} 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve
+## calculates inverse of a cacheMatrix when the value of cached Inverse is NULL
+## returns the cached inverse otherwise
+
+cacheSolve <- function(M, ...){
+    INV <- M$getINV() # read cached Inverse value of the cacheMatrix
+    if (!is.null(INV)) {
+        message("getting cached data")
+        return(INV) # returns the read value of cached inverse if it is not NULL
+    }
+    data <- M$get()
+    INV <- solve(data,...) # calculates inverse of cacheMatrix
+    M$setINV(INV) # sets cached Inverse value
+    INV # returns calculated inverse
+    
 }
